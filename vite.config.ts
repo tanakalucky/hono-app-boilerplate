999d38d77fbdd6efdfe6ec98e52b8c17e5e9991f
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2026-02-11 09:25:08 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path from "node:path";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), cloudflare(), tailwindcss()],
